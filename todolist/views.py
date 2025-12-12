0ab880ce799e9e6b3baadb1cde9adc3c68a33a93
Saroj Shrestha <shresthasaroj7777@gmail.com>
2025-12-12 15:38:51 +0545

    Url and views

---
+++ todolist/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def first(request):
+   return HttpResponse("<h1>Welcome</h1>")
+def home(request):
+   return HttpResponse("<h1>This is the Home Page</h1>")
